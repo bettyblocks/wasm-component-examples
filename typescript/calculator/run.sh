3c0ffaf77e23a4f943270fc2ab28f2eb55adcef1
Thomas Timmer <thomas.timmer@bettyblocks.com>
2025-10-28 15:41:57 +0100

    chore: disable jco all features

--- typescript/calculator/run.sh
+++ typescript/calculator/run.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # example how to run the wasm component
-wasmtime run -S http --invoke 'add(123, 7127)' calculator.wasm
-wasmtime run -S http --invoke 'subtract(123, 7127)' calculator.wasm
-wasmtime run -S http --invoke 'multiply(123, 7127)' calculator.wasm
-wasmtime run -S http --invoke 'divide(123, 7127)' calculator.wasm
+wasmtime run --invoke 'add(123, 7127)' calculator.wasm
+wasmtime run --invoke 'subtract(123, 7127)' calculator.wasm
+wasmtime run --invoke 'multiply(123, 7127)' calculator.wasm
+wasmtime run --invoke 'divide(123, 7127)' calculator.wasm
