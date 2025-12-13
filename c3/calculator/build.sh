42371ebd0c48209f105353bdb0d9164908a9b4d3
Thomas Timmer <thomastimmer11@hotmail.com>
2025-12-13 23:42:26 +0100

    chore: remove unused folders and settings

--- c3/calculator/build.sh
+++ c3/calculator/build.sh
@@ -4,7 +4,5 @@ WASI_SDK_DIR=/opt/wasi-sdk
 CC=${WASI_SDK_DIR}/bin/clang
 
 c3c build
-c3c compile-only --target wasm32 --output-dir build src/component.c3
 
-# ${CC} -target wasm32-wasip2 -mexec-model=reactor -Wunused main.c component.c main_component_type.o -o calculator.wasm
 ${CC} -target wasm32-wasip2 -mexec-model=reactor -Wunused build/calculator.a generated/main.c generated/main_component_type.o -o calculator.wasm
